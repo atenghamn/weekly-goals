c09aefa93987665889b355f39c6d8345159a328b
Alexander Tenghamn <alex.tenghamn@gmail.com>
2024-03-04 19:41:32 +0100

    Wasnt allowed to evict cache on / so rolling back changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,6 @@ import { revalidatePath } from 'next/cache';
 export default async function IndexPage() {
   
   const weeklyGoals = await fetchWeeklyGoals();
-  if (weeklyGoals.length !== 0) {
-    revalidatePath('/')
-  }
 
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
